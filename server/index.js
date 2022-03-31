802d602064f37b4efb6a501ba2dfb6c40d809f66
NathanInnocentAmy <nathaninnocent@gmail.com>
2022-03-30 21:18:18 -0400

    Added getItems & getItem api & serverResponse file

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const morgan = require("morgan");
 
 const PORT = 4000;
 // Nathan's API file
-const { handler1 } = require("./handler1");
+const { getItems, getItem, updateItemsNathan } = require("./handler1");
+const { getCompanies, getCompany, updateItems } = require("./handler2");
 
 express()
   .use(function (req, res, next) {
@@ -24,6 +25,21 @@ express()
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
   .use("/", express.static(__dirname + "/"))
+
+  // ======================== List of all enpoints ========================//
+
+  // Gets list of all items in database
+  .get("/api/get-items", getItems)
+  // Gets list of specific item in database
+  .get("/api/get-items/:itemId", getItem)
+  // Gets list of all companies in database
+  .get("/api/get-companies", getCompanies)
+  // Gets list of specific company in database
+  .get("/api/get-companies/:companyId", getCompany)
+  // Updates item when user buys items Danqi Version
+  .patch("/api/update-items", updateItems)
+  // Updates item when user buys items Nathan Version
+  .patch("/api/update-items", updateItemsNathan)
 
   // Handles all the endpoints
   .get("*", (req, res) => {
