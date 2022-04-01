aa8ef9f3b9444825919aa144af3d8f88eb1f3f04
Dan Qi Liu <ldq637tw@yahoo.ca>
2022-04-01 08:38:42 -0400

    adding search APIs

--- server/handler2.js
+++ server/handler2.js
@@ -94,4 +94,117 @@ const updateItems = async (req, res) => {
   }
   client.close();
 };
-module.exports = { getCompanies, getCompany, updateItems };
+
+//search items base on keyword (case insensitive)
+const searchItems = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const keywords = new RegExp(req.query.keywords, "i");
+  console.log(typeof keywords);
+  try {
+    await client.connect();
+    const db = client.db("ecommerce");
+    const result = await db
+      .collection("items")
+      .find({
+        $or: [
+          { name: { $regex: keywords } },
+          { body_location: { $regex: keywords } },
+          { category: { $regex: keywords } },
+        ],
+      })
+      .toArray();
+    if (result.length) {
+      res
+        .status(200)
+        .json({ status: 200, data: result, message: "search succeeded" });
+    } else {
+      res.status(404).json({
+        status: 404,
+        data: [req.query.keywords],
+        message: `No matched items for ${req.query.keywords}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      data: [req.query.keywords],
+      message: err.message,
+    });
+  }
+  client.close();
+};
+
+//search items for specified category name
+const searchCategory = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const category = req.params.categoryName;
+  const keyword = new RegExp(category, "i");
+  try {
+    await client.connect();
+    const db = client.db("ecommerce");
+    const result = await db
+      .collection("items")
+      .find({ category: { $regex: keyword } })
+      .toArray();
+    if (result.length) {
+      res
+        .status(200)
+        .json({ status: 200, data: result, message: "search succeeded" });
+    } else {
+      res.status(404).json({
+        status: 404,
+        data: [category],
+        message: `No matched items for ${category}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      data: [category],
+      message: err.message,
+    });
+  }
+  client.close();
+};
+
+//search items for specified body location
+const searchBodyLocation = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const bodyLocation = req.params.bodyLocation;
+  const keyword = new RegExp(bodyLocation, "i");
+  try {
+    await client.connect();
+    const db = client.db("ecommerce");
+    const result = await db
+      .collection("items")
+      .find({ body_location: { $regex: keyword } })
+      .toArray();
+    if (result.length) {
+      res
+        .status(200)
+        .json({ status: 200, data: result, message: "search succeeded" });
+    } else {
+      res.status(404).json({
+        status: 404,
+        data: [bodyLocation],
+        message: `No matched items for ${bodyLocation}`,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 500,
+      data: [bodyLocation],
+      message: err.message,
+    });
+  }
+  client.close();
+};
+
+module.exports = {
+  getCompanies,
+  getCompany,
+  updateItems,
+  searchItems,
+  searchCategory,
+  searchBodyLocation,
+};
