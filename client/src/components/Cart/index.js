620e2ae2bec5333db0b8a1857b12be35117b386e
Nathan Innocent Amy <89917308+OnePunchKo@users.noreply.github.com>
2022-04-04 11:12:58 -0400

    Update index.js

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,7 +8,6 @@ import { NavLink } from "react-router-dom";
 
 import styled from "styled-components";
 import { ShoppingCartContext } from "../Context/ShoppingCartContext";
-import { CheckoutForm } from "../Forms/CheckoutForm";
 
 import Item from "../ListingGrid/Item";
 import CartItem from "./CartItem";
